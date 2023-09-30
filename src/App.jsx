2dc46de905e680373c9df729e498328ea4e0f2c2
alireza mohseni <al.mohseni@ernyka.com>
2023-09-30 10:31:50 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect } from "react";
+
+function App() {
+  const videoRef = useRef(null);
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  };
+
+  return (
+    <div>
+      <button>Take a photo</button>
+      <video ref={videoRef} />
+      <canvas />
+    </div>
+  );
+}
+
+export default App;
